706cffd6c01a1329755c92cd10dd3d0257c9ca00
edddyguo <edddyguo@gmail.com>
2022-01-26 18:20:37 +0800

    chore: +1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chemix backend
+
+
+##TODO
+```
+1、api接口定位，提供假数据
+2、撮合逻辑实现
+3、swagger的替代在线文档
+4、enther交互sdk调试
+5、数据库字段和索引设计
+6、定时进行数据加工的逻辑
+7、测试用例
+8、机器人
+8、actix_web_actor ws
+```
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,8 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
 
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", name, id)
-    
 }
 
 #[get("/listMarkets")]
@@ -21,13 +20,25 @@ async fn freeze_balance(web::Path((user)): web::Path<(String)>) -> impl Responde
     format!("Hello {}! id:{}", user, 10)
 }
 
+async fn add_market(web::Path((contract_address)): web::Path<(String)>) -> HttpResponse {
+    HttpResponse::Ok().body(contract_address)
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index)
-        .service(list_markets)
-        .service(dex_info)
-    )
+    HttpServer::new(|| {
+        App::new().service(index)
+            .service(list_markets)
+            .service(dex_info)
+            .service(freeze_balance)
+            .service(web::resource("/addMarket/{contract_address}").route(web::post().to(add_market)))
+            .service(echo)
+    })
         .bind("127.0.0.1:8080")?
         .run()
         .await
