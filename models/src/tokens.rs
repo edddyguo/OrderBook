d3a68efca1d4fe69909be72c79178fc8d4021c73
edddyguo <edddyguo@gmail.com>
2022-03-05 20:33:48 +0800

    +1

--- models/src/tokens.rs
+++ models/src/tokens.rs
@@ -48,3 +48,20 @@ pub fn list_tokens() -> Vec<Token> {
     }
     tokens
 }
+
+pub fn get_token(symbol:&str) -> Token {
+
+    let sql = format!("select symbol,name,address,front_decimals,\
+    base_contract_decimal,cvt_url,show_cvt from chemix_tokens where symbol='{}'",symbol);
+    let rows = crate::query(sql.as_str()).unwrap();
+    info!("get_snapshot: raw sql {}", sql);
+    Token {
+        symbol: rows[0].get(0),
+        name: rows[0].get(1),
+        address: rows[0].get(2),
+        front_decimals: rows[0].get(3),
+        base_contract_decimal: rows[0].get(4),
+        cvt_url: rows[0].get(5),
+        show_cvt: rows[0].get(6),
+    }
+}
