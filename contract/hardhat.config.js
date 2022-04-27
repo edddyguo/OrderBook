7f9e01b0479d2b103694b78f3c8a5679397bf7b0
edddyguo <edddyguo@gmail.com>
2022-04-27 10:43:44 +0800

    acala_test init

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -4,7 +4,7 @@ require('@openzeppelin/hardhat-upgrades');
 
 //hardhat项目配置项
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
     tbsc: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545', //bsc test net
@@ -43,6 +43,22 @@ module.exports = {
     },
     peth: {
       url: 'http://192.168.1.158:8548', //本地RPC地址
+      //本地区块链账户地址(需要启动运行npx hardhat node命令开启本地开发环境的区块链)
+      //这些账户地址和秘钥每次重启区块链都是相同的,并且数据会重置
+      accounts: [
+        // 0x613548d151E096131ece320542d19893C4B8c901 chemix-test1
+        '0xa26660eb5dfaa144ae6da222068de3a865ffe33999604d45bd0167ff1f4e2882',
+        //'0x1f3bc7d273c179f0b73745d0599a15ece081837a9aa4ccb6351842fcad19fb95',
+        // 0x37BA121cdE7a0e24e483364185E80ceF655346DD chemix-test2
+        '0xb89da4744ef5efd626df7c557b32f139cdf42414056447bba627d0de76e84c43',
+        // 0xca9B361934fc7A7b07814D34423d665268111726 chemix-test3
+        '0xb0a09e85dad814ccc7231982401cca5accc3a46bc68349b403a7a129517cc266',
+        //0xF668b864756a2fB53b679bb13e0F9AB2d9C5fEE0  chemix-test4
+        '3bf8a9797398ee2dcdd550bad07b73c41a9af7c94be3aa97cdebde8c0efef00b'
+      ]
+    },
+    mandala: {
+      url: 'https://tc7-eth.aca-dev.network', //本地RPC地址
       //本地区块链账户地址(需要启动运行npx hardhat node命令开启本地开发环境的区块链)
       //这些账户地址和秘钥每次重启区块链都是相同的,并且数据会重置
       accounts: [
